915be6b75f561916f02105e308650c6b0eba5f85
nphiukhao <nphiukhao@gmail.com>
2019-08-12 13:07:26 -0700

    finished drill1

--- drill1.js
+++ drill1.js
@@ -1,10 +1,28 @@
-//console.log("Hi, my name is Nancy and I'm 23.");
-
 function createGreeting(name, age){
     
+    if(name === undefined || age === undefined){
+        throw new Error("You're invalid");
+    }
+
+    if(typeof age != "number" || typeof name != "string") {
+        throw new Error("not a real number or real person");
+    }
+
     const yearOfBirth = 2019 - age;
     return 'Hi my name is ' + name + ' and I was born in ' + yearOfBirth + '!';
 }
-let nameOne = createGreeting('stew',26);
+let nameOne = createGreeting("29", 34);
 
 console.log(nameOne)
+
+function getYearOfBirth(age){
+
+    if(age < 0){
+        throw console.error("Age can't be negative");
+    }
+
+   
+    return 2019 - age;
+}
+
+getYearOfBirth(-22);
