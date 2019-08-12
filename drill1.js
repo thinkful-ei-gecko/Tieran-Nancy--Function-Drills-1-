68f207df11ab0d37b8feb046f2dda2110ab9a516
Tieran Dysart <tierandysart@gmail.com>
2019-08-12 13:39:58 -0600

    added age subtraction var to function

--- drill1.js
+++ drill1.js
@@ -1,6 +1,10 @@
 //console.log("Hi, my name is Nancy and I'm 23.");
 
-function createGreeting(name){
-    return 'Hi my name is ' + name ;
+function createGreeting(name, age){
+    
+    const yearOfBirth = 2019 - age;
+    return 'Hi my name is ' + name + ' and I was born in ' + yearOfBirth + '!';
 }
-createGreeting('stew');
+let nameOne = createGreeting('stew',26);
+
+console.log(nameOne)
