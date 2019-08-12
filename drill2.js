279a9ad2690b4284fcd4dd18b71c5ff00d2dcf03
Tieran Dysart <tierandysart@gmail.com>
2019-08-12 15:41:01 -0600

    RPS not completed

--- drill2.js
+++ drill2.js
@@ -29,9 +29,22 @@ function beyond(num){
 beyond(0);
 
 function decode(word){
-    if(word[0]=== ''){}
-    if(word[0]=== ''){}
-    if(word[0]=== ''){}
-    if(word[0]=== ''){}
+    
+    if(word[0]=== 'a'){
+        return word[2]
+    }
+     else if(word[0]=== 'b'){
+        return word[3]
+    }
+    else if(word[0]=== 'c'){
+        return word[4]
+    }
+    else if(word[0]=== 'd'){
+        return word[5]
+    }
+    else{
+        return '';
+    }
 }
-for loop
+}
+
