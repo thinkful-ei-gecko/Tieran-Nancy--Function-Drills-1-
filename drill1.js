5635f32e2690a797c0228498b81ff86ba14b574f
nphiukhao <nphiukhao@gmail.com>
2019-08-12 12:28:29 -0700

    returned function

--- drill1.js
+++ drill1.js
@@ -1,6 +1,6 @@
-console.log("Hi, my name is Nancy and I'm 23.");
+//console.log("Hi, my name is Nancy and I'm 23.");
 
 function createGreeting(name){
-    console.log('Hi my name is ' + name);
+    return 'Hi my name is ' + name ;
 }
-createGreeting('Stew');
+createGreeting('stew');
